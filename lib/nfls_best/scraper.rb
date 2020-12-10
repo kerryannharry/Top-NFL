8a69f3613a7a2413a947ea7c41098f54c1ae066f
KerryAnn Harry <kerryann.harry@yahoo.com>
2020-12-10 13:50:45 -0600

    Scaper class/file with multiple methods to get teams/players/ranks

--- lib/nfls_best/scraper.rb
+++ lib/nfls_best/scraper.rb
@@ -3,19 +3,18 @@ class Scraper
     def self.top_teams
         html = URI.open(URL)
         doc = Nokogiri::HTML(html)
-teams = doc.css("#main-content > article > div:nth-child(2) > div > div > div.nfl-o-ranked-item > div.nfl-o-ranked-item__content > div.nfl-o-ranked-item__title").text
-ranks = doc.search("div.nfl-o-ranked-item__label--second").text
-        teams
-# teams.each do |team|
-# end
     end
 
-URL = "https://www.espn.com/nfl/story/_/id/29766900/nfl-rank-predicting-best-100-players-2020-nfl-season"
-    def self.top_players
-        html = URI.open(URL)
-        doc = Nokogiri::HTML(html)
-players = doc.css("#article-feed > article:nth-child(1) > div > div.article-body > h2:nth-child(13)").text #may be just number 1 player and rank
-# ranks = doc.search("div.nfl-o-ranked-item__label--second").text
-        player
+    def get_teams
+teams = doc.css("#main-content > article > div:nth-child(2) > div > div > div.nfl-o-ranked-item > div.nfl-o-ranked-item__content > div.nfl-o-ranked-item__title").text
     end
+
+    def get_team_rank
+ranks = doc.search("div.nfl-o-ranked-item__label--second").text
+    end
+    
+    def get_players
+players = doc.search("#main-content > article > div:nth-child(2) > div > div:nth-child(3) > div.nfl-o-ranked-item > div.nfl-o-ranked-item__content > div.nfl-o-ranked-item__title")
+    end
+
 end
