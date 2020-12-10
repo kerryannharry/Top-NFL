77bb2a134a780875ff428d143bd75eeebbad79ed
KerryAnn Harry <kerryann.harry@yahoo.com>
2020-12-10 06:30:42 -0600

    started player scraper. binding pry won't show data. doc.css w/ top players may change

--- lib/nfls_best/scraper.rb
+++ lib/nfls_best/scraper.rb
@@ -5,8 +5,17 @@ class Scraper
         doc = Nokogiri::HTML(html)
 teams = doc.css("#main-content > article > div:nth-child(2) > div > div > div.nfl-o-ranked-item > div.nfl-o-ranked-item__content > div.nfl-o-ranked-item__title").text
 ranks = doc.search("div.nfl-o-ranked-item__label--second").text
-        team
+        teams
 # teams.each do |team|
 # end
+    end
+
+URL = "https://www.espn.com/nfl/story/_/id/29766900/nfl-rank-predicting-best-100-players-2020-nfl-season"
+    def self.top_players
+        html = URI.open(URL)
+        doc = Nokogiri::HTML(html)
+players = doc.css("#article-feed > article:nth-child(1) > div > div.article-body > h2:nth-child(13)").text #may be just number 1 player and rank
+# ranks = doc.search("div.nfl-o-ranked-item__label--second").text
+        player
     end
 end
