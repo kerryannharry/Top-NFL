d9041669883ffe806aa73fa0a8d21ef89f654fb7
KerryAnn Harry <kerryann.harry@yahoo.com>
2020-12-21 13:39:51 -0600

    refactor to team class

--- lib/nfls_best/team.rb
+++ lib/nfls_best/team.rb
@@ -16,18 +16,13 @@ attr_accessor :name, :rank, :description
     def players
         NflsBest::Player.all.select {|player| player.team == self }
     end
+
     def display_roster
         roster = TTY::Table.new(header: ["Id", "Name", "Position", "Height", "Weight", "Experience", "College"])
         self.players.each.with_index(1) do |player, i| 
             roster << ["#{i}".red, "#{player.name}", "#{player.position}", "#{player.height}", "#{player.weight}", "#{player.experience}", "#{player.college}"]
         end
         puts roster.render(:unicode)
-    end
-    def self.display_teams
-        rank_teams = all.select {|team| team.rank != "not top 10"}
-        rank_teams.sort_by {|t| t.rank.to_i}.each do |team| 
-            puts "#{team.rank}. #{team.name}"
-        end
     end
 
     def self.display_grid_of_teams(teams)
@@ -41,6 +36,7 @@ attr_accessor :name, :rank, :description
     def self.find_by_name(name)
         self.all.find {|team| team.name.downcase == name.downcase}
     end
+    
     def self.find_or_create_by_name(name)
         self.find_by_name(name) || self.new(name)
     end
